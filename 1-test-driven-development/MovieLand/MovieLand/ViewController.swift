5bf6cb18960871e218655fed07cea1877423852a
Mehmet Tarhan <hi@memtarhan.com>
2021-04-04 21:47:10 +0300

    [Test Driven Development] - Setup project (Movie Land)

---
+++ 1-test-driven-development/MovieLand/MovieLand/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MovieLand
+//
+//  Created by Mehmet Tarhan on 4.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
