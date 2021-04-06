212533f4f69107ad17346037d1dd79000825e64f
Mehmet Tarhan <hi@memtarhan.com>
2021-04-06 20:28:44 +0300

    [Persistence-Realm] - Added Realm via CocoaPods

--- 2-persistence-realm/NoteLand/NoteLand/ViewController.swift
+++ 2-persistence-realm/NoteLand/NoteLand/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Realm 
 
 class ViewController: UIViewController {
 
---
+++ 2-persistence-realm/NoteLand/Pods/Target Support Files/Pods-NoteLandTests/Pods-NoteLandTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NoteLandTests : NSObject
+@end
+@implementation PodsDummy_Pods_NoteLandTests
+@end
