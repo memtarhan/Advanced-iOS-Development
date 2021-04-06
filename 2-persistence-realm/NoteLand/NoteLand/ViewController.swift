366551b00d6843b5c34655e52b20e56342948630
Mehmet Tarhan <hi@memtarhan.com>
2021-04-06 20:18:26 +0300

    [Persistence-Realm] - Setup project (Note Land)

---
+++ 2-persistence-realm/NoteLand/NoteLand/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteLand
+//
+//  Created by Mehmet Tarhan on 6.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
