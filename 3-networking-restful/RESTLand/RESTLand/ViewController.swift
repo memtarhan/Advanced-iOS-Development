a5e98bc92f07f58ee318beb4f1127db222d09195
Mehmet Tarhan <hi@memtarhan.com>
2021-04-08 23:50:28 +0300

    [Networking-RESTful] - A basic fetch from a url with URLSession

--- 3-networking-restful/RESTLand/RESTLand/ViewController.swift
+++ 3-networking-restful/RESTLand/RESTLand/ViewController.swift
@@ -11,9 +11,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // fetch()
     }
 
+    
+    private func fetch() {
+        let urlString = "https://www.google.com"
+        if let url = URL(string: urlString) {
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                let dataString = String(data: data!, encoding: .ascii)
+                print("Fetched: \(String(describing: dataString))")
+            }
+            
+            task.resume()
+        }
+    }
 
 }
 
