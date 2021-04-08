ca054da3a4e776e6bd624a44461b9fd10cda36a9
Mehmet Tarhan <hi@memtarhan.com>
2021-04-08 23:45:58 +0300

    [Networking-RESTful] - Setup project (REST Land)

---
+++ 3-networking-restful/RESTLand/RESTLand/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RESTLand
+//
+//  Created by Mehmet Tarhan on 8.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
