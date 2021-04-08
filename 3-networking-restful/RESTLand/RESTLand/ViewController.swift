0275ddf938fd947e19b113d496d26b1f55ad2cd1
Mehmet Tarhan <hi@memtarhan.com>
2021-04-08 23:59:28 +0300

    [Networking-RESTful] - Fetching data from an API

--- 3-networking-restful/RESTLand/RESTLand/ViewController.swift
+++ 3-networking-restful/RESTLand/RESTLand/ViewController.swift
@@ -9,19 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let baseURL = "https://www.orangevalleycaa.org/api"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // fetch()
+        fetch(withId: 1)
     }
 
     
     private func fetch() {
-        let urlString = "https://www.google.com"
+        let urlString = "\(baseURL)/music"
         if let url = URL(string: urlString) {
             let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
                 let dataString = String(data: data!, encoding: .ascii)
                 print("Fetched: \(String(describing: dataString))")
+            }
+            
+            task.resume()
+        }
+    }
+    
+    private func fetch(withId id: Int) {
+        let urlString = "\(baseURL)/music/\(id)"
+        if let url = URL(string: urlString) {
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                let dataString = String(data: data!, encoding: .ascii)
+                print("Fetched with Id: \(String(describing: dataString))")
             }
             
             task.resume()
