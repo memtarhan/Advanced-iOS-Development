4e057adae4056b107fe0ac405670b7fb7b489811
Mehmet Tarhan <hi@memtarhan.com>
2021-04-18 00:07:37 +0300

    [CoreLocation & MapKit] - Setup a map

--- 4-core-location-mapkit/Land/Land/ViewController.swift
+++ 4-core-location-mapkit/Land/Land/ViewController.swift
@@ -5,15 +5,36 @@
 //  Created by Mehmet Tarhan on 17.04.2021.
 //
 
+import MapKit
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet var mapTypeButton: UIButton!
+    @IBOutlet var pitchButton: UIButton!
+    @IBOutlet var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    // MARK: - Actions
+    @IBAction func didTapMapType(_ sender: Any) {
+    }
 
+    @IBAction func didTapPitch(_ sender: Any) {
+    }
+
+    @IBAction func didTapFeatures(_ sender: Any) {
+    }
+
+    @IBAction func didTapHere(_ sender: Any) {
+    }
+
+    @IBAction func didTapFind(_ sender: Any) {
+    }
+
+    @IBAction func didChangeLocation(_ sender: UISegmentedControl) {
+    }
 }
-
