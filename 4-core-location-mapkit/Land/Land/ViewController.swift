e99b8be05c1c3dc242b77d8c52aa3e127e98d61a
Mehmet Tarhan <hi@memtarhan.com>
2021-04-17 23:38:26 +0300

    [CoreLocation & MapKit] - Setup project (Land)

---
+++ 4-core-location-mapkit/Land/Land/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Land
+//
+//  Created by Mehmet Tarhan on 17.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
