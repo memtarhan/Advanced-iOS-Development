01883bb8fe3a8e8529ef8a85fe42bf8191ca82c9
Mehmet Tarhan <hi@memtarhan.com>
2021-04-18 00:19:29 +0300

    [CoreLocation & MapKit] - Add coordinates and regions

--- 4-core-location-mapkit/Land/Land/ViewController.swift
+++ 4-core-location-mapkit/Land/Land/ViewController.swift
@@ -5,21 +5,44 @@
 //  Created by Mehmet Tarhan on 17.04.2021.
 //
 
+/* --- Coordinate information -----
+            Lat,long
+ Naples: 40.8367321,14.2468856
+ New York: 40.7216294 , -73.995453
+ Chicago: 41.892479 , -87.6267592
+ Chatham: 42.4056555,-82.1860369
+ Beverly Hills: 34.0674607,-118.3977309
+
+ -->Challenges!!<----
+ 208 S. Beverly Drive Beverly Hills CA:34.0647691,-118.3991328
+ 2121 N. Clark St Chicago IL: 41.9206921,-87.6375361
+
+ For region monitoring:
+ latitude: 37.33454235, longitude: -122.03666775000001
+ --- */
+
 import MapKit
 import UIKit
 
 class ViewController: UIViewController {
-    
     // MARK: - Outlets
+
     @IBOutlet var mapTypeButton: UIButton!
     @IBOutlet var pitchButton: UIButton!
     @IBOutlet var mapView: MKMapView!
 
+    // MARK: - Properties
+
+    var coordinate2D = CLLocationCoordinate2DMake(40.8367321, 14.2468856)
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        updateMapRegion(rangeSpan: 100)
     }
 
     // MARK: - Actions
+
     @IBAction func didTapMapType(_ sender: Any) {
     }
 
@@ -36,5 +59,30 @@ class ViewController: UIViewController {
     }
 
     @IBAction func didChangeLocation(_ sender: UISegmentedControl) {
+        let index = sender.selectedSegmentIndex
+
+        switch index {
+        case 0: // Naples
+            coordinate2D = CLLocationCoordinate2DMake(40.8367321, 14.2468856)
+        case 1: // New York
+            coordinate2D = CLLocationCoordinate2DMake(40.7216294, -73.995453)
+        case 2: // Chicago
+            coordinate2D = CLLocationCoordinate2DMake(41.892479, -87.6267592)
+        case 3: // Chatham
+            coordinate2D = CLLocationCoordinate2DMake(42.4056555, -82.1860369)
+        case 4: // Beverly Hills
+            coordinate2D = CLLocationCoordinate2DMake(34.0674607, -118.3977309)
+        default: // Naples
+            coordinate2D = CLLocationCoordinate2DMake(40.8367321, 14.2468856)
+        }
+
+        updateMapRegion(rangeSpan: 100)
+    }
+
+    // MARK: Instance Methods
+
+    func updateMapRegion(rangeSpan: CLLocationDistance) {
+        let region = MKCoordinateRegion(center: coordinate2D, latitudinalMeters: rangeSpan, longitudinalMeters: rangeSpan)
+        mapView.region = region
     }
 }
