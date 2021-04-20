d63a1da3e39269e3192cbf3742e6544ab09b9814
Mehmet Tarhan <hi@memtarhan.com>
2021-04-20 19:19:23 +0300

    [CoreLocation & MapKit] - Geocoding and placemark data

--- 4-core-location-mapkit/Land/Land/AnnotationDetailViewController.swift
+++ 4-core-location-mapkit/Land/Land/AnnotationDetailViewController.swift
@@ -6,24 +6,57 @@
 //  Copyright © 2017 Steven Lipton. All rights reserved.
 //
 
+import MapKit
 import UIKit
 
 class AnnotationDetailViewController: UIViewController {
     var annotation: PizzaAnnotation!
-    
+
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var pizzaPhoto: UIImageView!
     @IBOutlet var historyText: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        titleLabel.text = annotation.title
+        pizzaPhoto.image = annotation.photo
+        historyText.text = annotation.history
+
+        retrievePlacemark(annotation) { placemark in
+            if let placemark = placemark {
+                var locationString = ""
+                if let city = placemark.locality {
+                    locationString += "City: \(city) - "
+                }
+                if let state = placemark.administrativeArea {
+                    locationString += "State: \(state) - "
+                }
+                if let country = placemark.country {
+                    locationString += "Country: \(country)"
+                }
+
+                print("Placemark: \(locationString)")
+            }
+        }
+    }
 
     @IBAction func done(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        titleLabel.text = annotation.title
-        pizzaPhoto.image = annotation.photo
-        historyText.text = annotation.history
-        // Do any additional setup after loading the view.
+    func retrievePlacemark(_ annotation: PizzaAnnotation, completionHandler: @escaping (CLPlacemark?) -> Void) {
+        let coordinate = annotation.coordinate
+        let location = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(location) { placemarks, error in
+            if let placemarks = placemarks {
+                completionHandler(placemarks.first)
+
+            } else {
+                completionHandler(nil)
+                print("Error on Geocoder: \(error!.localizedDescription)")
+            }
+        }
     }
 }
