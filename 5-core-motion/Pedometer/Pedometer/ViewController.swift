2af22dae37137745082ac9ef4986c34105838301
Mehmet Tarhan <hi@memtarhan.com>
2021-04-22 11:30:23 +0300

    [CoreMotion] - Add the pedometer

--- 5-core-motion/Pedometer/Pedometer/ViewController.swift
+++ 5-core-motion/Pedometer/Pedometer/ViewController.swift
@@ -5,15 +5,51 @@
 //  Created by Mehmet Tarhan on 22.04.2021.
 //
 
+import CoreMotion
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: - Outlets
+
+    @IBOutlet var statusLabel: UILabel!
+    @IBOutlet var stepsLabel: UILabel!
+    @IBOutlet var distanceLabel: UILabel!
+    @IBOutlet var paceLabel: UILabel!
+    @IBOutlet var startButton: UIButton!
+
+    // MARK: - Properties
+
+    var pedometer = CMPedometer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    // MARK: - Actions
 
+    @IBAction func didTapStart(_ sender: UIButton) {
+        if sender.titleLabel?.text == "Start" {
+            statusLabel.text = "On"
+            sender.setTitle("Stop", for: .normal)
+
+            if CMPedometer.isStepCountingAvailable() {
+                pedometer.startUpdates(from: Date()) { data, error in
+                    if let error = error {
+                        print("Pedometer: error -> \(error.localizedDescription)")
+
+                    } else if let data = data {
+                        self.stepsLabel.text = "\(data.numberOfSteps)"
+                    }
+                }
+
+            } else {
+                print("No step counting")
+                stepsLabel.text = "Not Available"
+            }
+        } else {
+            pedometer.stopUpdates()
+            statusLabel.text = "Off"
+            sender.setTitle("Start", for: .normal)
+        }
+    }
 }
-
