891db496f60cf7d3a47541cfd49739657b8b7c5e
Mehmet Tarhan <hi@memtarhan.com>
2021-04-22 11:15:31 +0300

    [CoreMotion] - Setup project (Pedometer)

---
+++ 5-core-motion/Pedometer/Pedometer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pedometer
+//
+//  Created by Mehmet Tarhan on 22.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
