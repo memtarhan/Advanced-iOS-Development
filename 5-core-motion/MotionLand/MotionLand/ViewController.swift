411aeea3a5880e4a995639daf80b30176224536e
Mehmet Tarhan <hi@memtarhan.com>
2021-04-23 13:13:31 +0300

    [CoreMotion] - Detecting devices

--- 5-core-motion/MotionLand/MotionLand/ViewController.swift
+++ 5-core-motion/MotionLand/MotionLand/ViewController.swift
@@ -5,15 +5,34 @@
 //  Created by Mehmet Tarhan on 21.04.2021.
 //
 
+import CoreMotion
 import UIKit
 
 class ViewController: UIViewController {
+    let motionManager = CMMotionManager()
+
+    var isDevicesAvailable: Bool {
+//        let gyroAvailable = motionManager.isGyroActive
+//        let accelerometerAvailable = motionManager.isAccelerometerAvailable
+
+        if !motionManager.isDeviceMotionAvailable {
+            let alertController = UIAlertController(title: "Oopps", message: "Mo motion available", preferredStyle: .alert)
+//            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+//            alertController.addAction(cancelAction)
+            present(alertController, animated: true, completion: nil)
+        }
+        return motionManager.isDeviceMotionAvailable
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
 
+        if isDevicesAvailable {
+            print("CoreMotion launched")
+        }
+    }
 }
-
