2fa080fd2ee3f5e2400fcebfb42a5eb7d1706fab
Mehmet Tarhan <hi@memtarhan.com>
2021-04-30 01:09:54 +0300

    [Architecture][LifeCycle] - Walking through app life cycle

---
+++ 6-architecture/1-life-cycle/DemoApp/DemoApp/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  DemoApp
+//
+//  Created by Mehmet Tarhan on 30.04.2021.
+//
+
+import UIKit
+
+UIApplicationMain(CommandLine.argc, UnsafeMutableRawPointer(CommandLine.unsafeArgv).bindMemory(to: UnsafeMutablePointer<Int8>.self, capacity: Int(CommandLine.argc)), nil, NSStringFromClass(AppDelegate.self))
