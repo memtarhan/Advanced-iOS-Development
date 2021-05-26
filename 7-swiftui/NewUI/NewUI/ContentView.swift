e1e2e1679e2cdea6974ce9e24e44a470b2b9be75
Mehmet Tarhan <hi@memtarhan.com>
2021-05-27 01:25:29 +0300

    [SwiftUI] - Sliders

--- 7-swiftui/NewUI/NewUI/ContentView.swift
+++ 7-swiftui/NewUI/NewUI/ContentView.swift
@@ -8,17 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var name = ""
+    @State var red = 0.0
+    @State var green = 0.0
+    @State var blue = 0.0
 
     var body: some View {
         VStack {
-            TextField("Enter something...", text: $name)
-                .padding()
-            Text("Hello \(name)")
+            Text("Color Picker")
                 .font(.largeTitle)
                 .foregroundColor(Color.green)
                 .multilineTextAlignment(.center)
                 .padding()
+            Image(systemName: "scribble.variable")
+                .foregroundColor(Color(red: red, green: green, blue: blue, opacity: 1.0))
+                .font(.largeTitle)
+                .padding()
+
+            VStack {
+                Slider(value: $red, in: 0 ... 1, step: 0.01)
+                    .accentColor(/*@START_MENU_TOKEN@*/ .red/*@END_MENU_TOKEN@*/)
+                Slider(value: $green, in: 0 ... 1, step: 0.01)
+                    .accentColor(.green)
+                Slider(value: $blue, in: 0 ... 1, step: 0.01)
+                    .accentColor(.blue)
+            }
+            .padding()
         }
         .padding()
     }
