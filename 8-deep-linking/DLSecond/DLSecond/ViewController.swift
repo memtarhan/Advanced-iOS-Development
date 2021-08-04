b4518348422b592bf9d4bbb1fb83abbeed1c2f7b
Mehmet Tarhan <hi@memtarhan.com>
2021-08-04 22:41:40 +0100

    [Deep Linking] - Created projects

---
+++ 8-deep-linking/DLSecond/DLSecond/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DLSecond
+//
+//  Created by Mehmet Tarhan on 04/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
