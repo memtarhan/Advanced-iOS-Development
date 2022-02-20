f50383c54c71df326825f766afc0fcbac6add6c9
Mehmet Tarhan <hi@memtarhan.com>
2022-02-20 00:27:59 +0000

    [Objective-C] - More on Xcode & Objective-C

---
+++ 9-objective-c/MyCompany/MyCompany/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MyCompany
+//
+//  Created by Mehmet Tarhan on 20/02/2022.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ 9-objective-c/MyCompany/MyCompany/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyCompany
+//
+//  Created by Mehmet Tarhan on 20/02/2022.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
