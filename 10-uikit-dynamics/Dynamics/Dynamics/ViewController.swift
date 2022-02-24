da1a63481a64e0c318bdddf8a89378b63579ebad
Mehmet Tarhan <hi@memtarhan.com>
2022-02-24 16:02:54 +0000

    [UIKit Dynamics] - [Gravity] - Created project

---
+++ 10-uikit-dynamics/Dynamics/Dynamics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dynamics
+//
+//  Created by Mehmet Tarhan on 24/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
