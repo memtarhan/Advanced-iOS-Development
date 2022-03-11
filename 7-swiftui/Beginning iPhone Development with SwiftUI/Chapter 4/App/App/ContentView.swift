db0f4e72ac81d9b9a4ee3bd1499e2798f3502641
Mehmet Tarhan <hi@memtarhan.com>
2022-03-11 22:10:26 +0000

    [SwiftUI] - Label View and Text border

--- 7-swiftui/Beginning iPhone Development with SwiftUI/Chapter 4/App/App/ContentView.swift
+++ 7-swiftui/Beginning iPhone Development with SwiftUI/Chapter 4/App/App/ContentView.swift
@@ -31,25 +31,39 @@ import SwiftUI
 
 // - Similar to the Text view is the Label view. While the Text view just displays a single string of text, the Label view can display both a string and an image side by side
 
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Label("House", systemImage: "house.fill")
+//            Label("Custom image", image: "custom")
+//
+//            // More customizable Label
+//            Label {
+//                Text("More customized")
+//                    .font(.title)
+//            } icon: {
+//                Image(systemName: "pencil")
+//                    .font(.title)
+//                    .opacity(0.25)
+//            }
+//
+//            // Icon only
+//            Label("Icon only", systemImage: "hare.fill")
+//                .labelStyle(.iconOnly)
+//        }
+//    }
+//}
+
+// MARK: - Adding a Border Around a Text or Label View
+// - To highlight both a Text and a Label view, you can add a border around them
 struct ContentView: View {
     var body: some View {
         VStack {
-            Label("House", systemImage: "house.fill")
-            Label("Custom image", image: "custom")
-
-            // More customizable Label
-            Label {
-                Text("More customized")
-                    .font(.title)
-            } icon: {
-                Image(systemName: "pencil")
-                    .font(.title)
-                    .opacity(0.25)
-            }
-
-            // Icon only
-            Label("Icon only", systemImage: "hare.fill")
-                .labelStyle(.iconOnly)
+            Text("A border around a Text view")
+                .border(Color.red, width: 3)
+            Label("A border around a Label view", systemImage: "folder.fill")
+                .padding()
+                .border(Color.green, width: 4)
         }
     }
 }
