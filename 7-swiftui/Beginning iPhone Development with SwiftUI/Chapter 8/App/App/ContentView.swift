40eece39e7f6ec8b873e8fe6436ad12c19deb052
Mehmet Tarhan <hi@memtarhan.com>
2022-03-13 00:02:16 +0000

    [SwiftUI] - Using a Color Picker

--- 7-swiftui/Beginning iPhone Development with SwiftUI/Chapter 8/App/App/ContentView.swift
+++ 7-swiftui/Beginning iPhone Development with SwiftUI/Chapter 8/App/App/ContentView.swift
@@ -9,35 +9,51 @@ import SwiftUI
 
 // MARK: - Using a Picker
 
-struct ContentView: View {
-    @State var stringChoice = ""
-    @State var integerChoice = 0
+// struct ContentView: View {
+//    @State var stringChoice = ""
+//    @State var integerChoice = 0
+//
+//    var body: some View {
+//        VStack(spacing: 64) {
+//            Picker(selection: $stringChoice, label: Text("Picker")) {
+//                Text("1").tag("one")
+//                Text("2").tag("two")
+//                Text("3").tag("three")
+//                Text("4").tag("four")
+//                Text("5").tag("five")
+//            }
+//            .pickerStyle(WheelPickerStyle())
+//
+//            Text("You chose \(stringChoice)")
+//
+//            Picker(selection: $integerChoice, label: Text("Picker")) {
+//                Text("1").tag(1)
+//                Text("2").tag(2)
+//                Text("3").tag(3)
+//                Text("4").tag(4)
+//                Text("5").tag(5)
+//            }
+//            .pickerStyle(InlinePickerStyle())
+//
+//            Text("You chose \(integerChoice)")
+//
+//        }
+//    }
+// }
 
+// MARK: - Using a Color Picker
+
+struct ContentView: View {
+    @State var myColor = Color.gray
     var body: some View {
         VStack(spacing: 64) {
-            Picker(selection: $stringChoice, label: Text("Picker")) {
-                Text("1").tag("one")
-                Text("2").tag("two")
-                Text("3").tag("three")
-                Text("4").tag("four")
-                Text("5").tag("five")
-            }
-            .pickerStyle(WheelPickerStyle())
-            
-            Text("You chose \(stringChoice)")
+            RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
+                .frame(width: 200, height: 100)
+                .foregroundColor(myColor)
 
-            Picker(selection: $integerChoice, label: Text("Picker")) {
-                Text("1").tag(1)
-                Text("2").tag(2)
-                Text("3").tag(3)
-                Text("4").tag(4)
-                Text("5").tag(5)
-            }
-            .pickerStyle(InlinePickerStyle())
-            
-            Text("You chose \(integerChoice)")
-
+            ColorPicker("Pick a color", selection: $myColor)
         }
+        .padding()
     }
 }
 
