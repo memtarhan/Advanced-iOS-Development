38eaaff0a01ade9d1463587b192647c14f9d405e
Mehmet Tarhan <hi@memtarhan.com>
2022-03-13 22:17:03 +0000

    [SwiftUI] - Displaying Array Data in a List

--- 7-swiftui/Beginning iPhone Development with SwiftUI/Chapter 13/App/App/ContentView.swift
+++ 7-swiftui/Beginning iPhone Development with SwiftUI/Chapter 13/App/App/ContentView.swift
@@ -7,22 +7,37 @@
 
 import SwiftUI
 
-// MARK: - Using List 
+// MARK: - Using List
+
+// struct ContentView: View {
+//    var body: some View {
+//        List {
+//            ForEach(1 ... 25, id: \.self) { index in
+//                Cell(title: "Animal #\(index)")
+//            }
+//        }
+//    }
+// }
+//
+// struct Cell: View {
+//    @State var title = ""
+//    var body: some View {
+//        Text(title)
+//            .foregroundColor(Color.purple)
+//    }
+// }
+
+// MARK: - Displaying Array Data in a List
+
 struct ContentView: View {
+    var myArray = ["Cat", "Dog", "Turtle", "Ferret", "Parrot", "Goldfish", "Lizard", "Canary", "Tarantula", "Hamster"]
+
     var body: some View {
         List {
-            ForEach(1 ... 25, id: \.self) { index in
-                Cell(title: "Animal #\(index)")
+            ForEach(0 ... myArray.count - 1, id: \.self) { index in
+                Text(myArray[index])
             }
         }
-    }
-}
-
-struct Cell: View {
-    @State var title = ""
-    var body: some View {
-        Text(title)
-            .foregroundColor(Color.purple)
     }
 }
 
