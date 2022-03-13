a95f3e075c0f5e16a412db7ca752761ef9028647
Mehmet Tarhan <hi@memtarhan.com>
2022-03-13 22:39:24 +0000

    [SwiftUI] - Adding Swipe Gestures to a List

--- 7-swiftui/Beginning iPhone Development with SwiftUI/Chapter 13/App/App/ContentView.swift
+++ 7-swiftui/Beginning iPhone Development with SwiftUI/Chapter 13/App/App/ContentView.swift
@@ -71,49 +71,93 @@ import SwiftUI
 
 // MARK: - Creating Groups in a List
 
-struct SectionHeading: Identifiable {
-    let name: String
-    let animalList: [Animals]
-    let id = UUID()
-}
+// struct SectionHeading: Identifiable {
+//    let name: String
+//    let animalList: [Animals]
+//    let id = UUID()
+// }
+//
+// struct Animals: Hashable, Identifiable {
+//    let pet: String
+//    let id = UUID()
+// }
+//
+// struct ContentView: View {
+//    var animals = [
+//        SectionHeading(name: "Mammals",
+//                       animalList: [
+//                           Animals(pet: "Cat"),
+//                           Animals(pet: "Dog"),
+//                           Animals(pet: "Ferret"),
+//                           Animals(pet: "Hamster")]),
+//        SectionHeading(name: "Reptiles",
+//                       animalList: [
+//                           Animals(pet: "Turtle"),
+//                           Animals(pet: "Lizard")]),
+//        SectionHeading(name: "Birds",
+//                       animalList: [
+//                           Animals(pet: "Parrot"),
+//                           Animals(pet: "Canary")]),
+//        SectionHeading(name: "Other",
+//                       animalList: [
+//                           Animals(pet: "Tarantula"),
+//                           Animals(pet: "Goldfish")]),
+//    ]
+//    var body: some View {
+//        List {
+//            ForEach(animals) { heading in
+//                Section(header: Text("\(heading.name) Section")) {
+//                    ForEach(heading.animalList) { creature in
+//                        Text(creature.pet)
+//                    }
+//                    .listRowSeparatorTint(.red)
+//                    .listRowSeparator(.hidden)
+//                }
+//            }
+//        }
+//    }
+// }
 
-struct Animals: Hashable, Identifiable {
-    let pet: String
-    let id = UUID()
-}
+// MARK: - Adding Swipe Gestures to a List
+
+/*
+ In iOS 15, SwiftUI also allows custom left to right and right to left gestures on List items. The first step is to add a .swipeActions modifier to the view that displays items in a List such as a Text view. Then define whether you want a .leading (left to right) or .trailing (right to left) swipe gesture like this:
+ .swipeActions(edge: .trailing)
+ */
 
 struct ContentView: View {
-    var animals = [
-        SectionHeading(name: "Mammals",
-                       animalList: [
-                           Animals(pet: "Cat"),
-                           Animals(pet: "Dog"),
-                           Animals(pet: "Ferret"),
-                           Animals(pet: "Hamster")]),
-        SectionHeading(name: "Reptiles",
-                       animalList: [
-                           Animals(pet: "Turtle"),
-                           Animals(pet: "Lizard")]),
-        SectionHeading(name: "Birds",
-                       animalList: [
-                           Animals(pet: "Parrot"),
-                           Animals(pet: "Canary")]),
-        SectionHeading(name: "Other",
-                       animalList: [
-                           Animals(pet: "Tarantula"),
-                           Animals(pet: "Goldfish")]),
-    ]
+    @State var array = ["Cat", "Dog", "Turtle", "Ferret", "Parrot", "Goldfish", "Lizard", "Canary", "Tarantula", "Hamster"]
+    @State var message = ""
+
     var body: some View {
-        List {
-            ForEach(animals) { heading in
-                Section(header: Text("\(heading.name) Section")) {
-                    ForEach(heading.animalList) { creature in
-                        Text(creature.pet)
-                    }
-                    .listRowSeparatorTint(.red)
+        NavigationView {
+            List {
+                ForEach(0 ... array.count - 1, id: \.self) { index in
+                    Text(array[index])
+                        .swipeActions(edge: .leading) {
+                            Button {
+                                message = "Item = \(array[index]) -- Index = \(index)"
+                            } label: {
+                                Image(systemName: "calendar.circle")
+                            }.tint(.yellow)
+                        }
                 }
+                .onDelete(perform: delete)
+                .onMove(perform: move)
             }
+            .toolbar {
+                EditButton()
+            }
+            .navigationTitle("Animals")
         }
+    }
+
+    func delete(at offsets: IndexSet) {
+        array.remove(atOffsets: offsets)
+    }
+
+    func move(from source: IndexSet, to destination: Int) {
+        array.move(fromOffsets: source, toOffset: destination)
     }
 }
 
