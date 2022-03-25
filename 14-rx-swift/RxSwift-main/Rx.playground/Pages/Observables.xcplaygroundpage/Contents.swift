43dc05e4170d116bab38217e29eaa011f4edf87f
Mehmet Tarhan <hi@memtarhan.com>
2022-03-25 23:57:44 +0000

    [RxSwift] - Observables: Disposing

--- 14-rx-swift/RxSwift-main/Rx.playground/Pages/Observables.xcplaygroundpage/Contents.swift
+++ 14-rx-swift/RxSwift-main/Rx.playground/Pages/Observables.xcplaygroundpage/Contents.swift
@@ -3,18 +3,34 @@ import RxSwift
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
+// MARK: - Dispose Bag
+
+// MARK: - Observables: Disposing
+
+/*
+ - Preventing retain cycles (memory issues)
+ - ARC: Helps memory management
+ */
+
+// Free up memory when we're done with it.
+// It will call it when error occurs or it's completed
+// helloSubscription.dispose()
+
+// Let's do it with a dispose bag
+let disposeBag = DisposeBag()
+
 // MARK: - Creating and Subscribing to Observables
 
 /*
  Observable.just(number) -> Creating an onservable of a single value
  */
 let numberSequence = Observable.just(5) // = Observable<Int>(5)
-let numberSubscription = numberSequence.subscribe(onNext: {
+let numberSubscription: () = numberSequence.subscribe(onNext: {
     print($0)
-})
+}).disposed(by: disposeBag)
 
 let helloSequence = Observable.from(["H", "e", "l", "l", "o"])
-let helloSubscription = helloSequence.subscribe { event in
+let helloSubscription: () = helloSequence.subscribe { event in
     switch event {
     case let .next(value):
         print(value)
@@ -23,4 +39,4 @@ let helloSubscription = helloSequence.subscribe { event in
     case .completed:
         print("completed")
     }
-}
+}.disposed(by: disposeBag)
