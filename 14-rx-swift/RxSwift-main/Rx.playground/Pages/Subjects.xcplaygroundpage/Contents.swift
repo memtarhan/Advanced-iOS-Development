74a54888af8573da6a81a50597e7c79283a40658
Mehmet Tarhan <hi@memtarhan.com>
2022-03-26 00:18:10 +0000

    [RxSwift] - BehaviorSubject

--- 14-rx-swift/RxSwift-main/Rx.playground/Pages/Subjects.xcplaygroundpage/Contents.swift
+++ 14-rx-swift/RxSwift-main/Rx.playground/Pages/Subjects.xcplaygroundpage/Contents.swift
@@ -30,3 +30,31 @@ let pubSubSubscription2 = publishSubject.subscribe(onNext: {
 })
 
 publishSubject.onNext("Both subs get this message")
+
+// MARK: - BehaviorSubject
+
+// Most recent value as well as what follows
+var behaviorSubject = BehaviorSubject(value: "Value A")
+
+let behaviorSubscription = behaviorSubject.subscribe(onNext: {
+    print("behaviorSubscription - \($0)")
+})
+
+behaviorSubject.onNext("Value B")
+
+let behaviorSubscription2 = behaviorSubject.subscribe(onNext: {
+    print("behaviorSubscription2 - \($0)")
+})
+
+behaviorSubject.onNext("Value C")
+
+/*
+ Prints out
+
+ behaviorSubscription - Value A
+ behaviorSubscription - Value B
+ behaviorSubscription2 - Value B
+ behaviorSubscription - Value C
+ behaviorSubscription2 - Value C
+
+ */
