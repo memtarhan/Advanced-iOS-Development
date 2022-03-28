d5197d0969458a5ea3e9bb928b2fa946d52af017
Mehmet Tarhan <hi@memtarhan.com>
2022-03-28 15:07:02 +0100

    [RxSwift] - Transformations: .flatMap

--- 14-rx-swift/RxSwift-main/Rx.playground/Pages/Transformations.xcplaygroundpage/Contents.swift
+++ 14-rx-swift/RxSwift-main/Rx.playground/Pages/Transformations.xcplaygroundpage/Contents.swift
@@ -19,3 +19,33 @@ Observable<NSNumber>.of(123, 45, 9, 5987)
         print($0)
     })
     .disposed(by: disposeBag)
+
+// MARK: - Transformations: .flatMap
+
+struct Game {
+    var price: Observable<Double>
+}
+
+var overwatch = Game(price: Observable.just(59.99))
+let starwal = Game(price: Observable.just(19.99))
+
+let gameSubject = PublishSubject<Game>()
+
+overwatch.price = Observable.just(14.99)
+
+gameSubject.subscribe(onNext: {
+    print($0.price.asObservable())
+})
+
+// - Converting it to type of Price
+gameSubject
+    .flatMap {
+        $0.price
+    }
+    .subscribe(onNext: {
+        print("Game price: \($0)")
+    })
+    .disposed(by: disposeBag)
+
+gameSubject.onNext(overwatch)
+gameSubject.onNext(starwal)
