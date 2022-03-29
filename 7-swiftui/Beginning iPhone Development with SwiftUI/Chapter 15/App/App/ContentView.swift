a4e02dca952ef980528ed854f29dd36c9b07b79d
Mehmet Tarhan <hi@memtarhan.com>
2022-03-29 14:45:40 +0100

    [SwiftUI] - Using an Outline Group

--- 7-swiftui/Beginning iPhone Development with SwiftUI/Chapter 15/App/App/ContentView.swift
+++ 7-swiftui/Beginning iPhone Development with SwiftUI/Chapter 15/App/App/ContentView.swift
@@ -36,14 +36,64 @@ import SwiftUI
 
 // MARK: - Using a Scroll View
 
+// struct ContentView: View {
+//    var body: some View {
+//        ScrollView(.vertical, showsIndicators: false, content: {
+//            ForEach(0 ..< 50) {
+//                Text("Item #\($0)")
+//            }
+//        })
+//            .padding()
+//    }
+// }
+
+// MARK: - Using an Outline Group
+
+/*
+ Outline Groups display arrays of objects that define relationships. To use an Outline Group, you need to do the following:
+
+ - Create a class that holds the data you want to display.
+ - Create an array that holds multiple objects defined by the class.
+ - Create an Outline Group that displays the data stored in the objects.
+ */
+
+class Species: Identifiable {
+    let id = UUID()
+    var name: String
+    var classification: [Species]?
+
+    init(name: String, classification: [Species]? = nil) {
+        self.name = name
+        self.classification = classification
+    }
+}
+
+var animals: [Species] = [
+    Species(name: "Mammal", classification: [
+        Species(name: "Dog", classification: [
+            Species(name: "Poodle"),
+            Species(name: "Collie"),
+            Species(name: "St. Bernard"),
+        ]),
+        Species(name: "Cat"),
+        Species(name: "Elephant"),
+        Species(name: "Whale"),
+    ]),
+    Species(name: "Bird", classification: [
+        Species(name: "Canary"),
+        Species(name: "Parakeet"),
+        Species(name: "Eagle"),
+    ]),
+]
+
 struct ContentView: View {
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false, content: {
-            ForEach(0 ..< 50) {
-                Text("Item #\($0)")
+        VStack {
+            OutlineGroup(animals, id: \.id, children: \.classification) { creature in
+                Text(creature.name)
             }
-        })
-            .padding()
+        }
+        .padding()
     }
 }
 
