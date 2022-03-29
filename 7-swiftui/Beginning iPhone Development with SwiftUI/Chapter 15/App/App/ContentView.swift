2166138c62895182b7e88282881e7ab20a1b1db7
Mehmet Tarhan <hi@memtarhan.com>
2022-03-29 14:36:33 +0100

    [SwiftUI] - Using a Scroll View

--- 7-swiftui/Beginning iPhone Development with SwiftUI/Chapter 15/App/App/ContentView.swift
+++ 7-swiftui/Beginning iPhone Development with SwiftUI/Chapter 15/App/App/ContentView.swift
@@ -12,25 +12,38 @@ import SwiftUI
 /*
  Like a stack, a Disclosure Group can hold a maximum of ten views. However, one or more of those views can be stacks that can hold multiple views. In addition, you can nest Disclosure Groups inside of other Disclosure Groups.
  */
-struct ContentView: View {
-    @State var sliderValue = 0.0
-    @State var message = ""
-    @State var flag = true
+// struct ContentView: View {
+//    @State var sliderValue = 0.0
+//    @State var message = ""
+//    @State var flag = true
+//
+//    var body: some View {
+//        DisclosureGroup("Expand Me") {
+//            Text("You typed = \(message)")
+//            TextField("Type here", text: $message)
+//                .padding()
+//            Text(flag ? "Toggle = true" : "Toggle = false")
+//            Toggle(isOn: $flag) {
+//                Text("Toggle")
+//            }.padding()
+//            Text("The slider value = \(sliderValue)")
+//            Slider(value: $sliderValue, in: 0 ... 15)
+//                .padding()
+//        }
+//        .padding()
+//    }
+// }
 
+// MARK: - Using a Scroll View
+
+struct ContentView: View {
     var body: some View {
-        DisclosureGroup("Expand Me") {
-            Text("You typed = \(message)")
-            TextField("Type here", text: $message)
-                .padding()
-            Text(flag ? "Toggle = true" : "Toggle = false")
-            Toggle(isOn: $flag) {
-                Text("Toggle")
-            }.padding()
-            Text("The slider value = \(sliderValue)")
-            Slider(value: $sliderValue, in: 0 ... 15)
-                .padding()
-        }
-        .padding()
+        ScrollView(.vertical, showsIndicators: false, content: {
+            ForEach(0 ..< 50) {
+                Text("Item #\($0)")
+            }
+        })
+            .padding()
     }
 }
 
