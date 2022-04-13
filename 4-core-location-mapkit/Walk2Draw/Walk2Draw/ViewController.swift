783615c4f57a4aeeb4224f523a1166433ceb9326
Mehmet Tarhan <hi@memtarhan.com>
2022-04-14 00:05:53 +0100

    [CoreLocation & MapKit] - Created Walk2Draw

---
+++ 4-core-location-mapkit/Walk2Draw/Walk2Draw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Walk2Draw
+//
+//  Created by Mehmet Tarhan on 14/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 4-core-location-mapkit/Walk2Draw/Walk2DrawUITests/Walk2DrawUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Walk2DrawUITestsLaunchTests.swift
+//  Walk2DrawUITests
+//
+//  Created by Mehmet Tarhan on 14/04/2022.
+//
+
+import XCTest
+
+class Walk2DrawUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
