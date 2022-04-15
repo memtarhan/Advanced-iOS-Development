bc7e3182a26738acd1b40e9900558d5270cf02a3
Mehmet Tarhan <hi@memtarhan.com>
2022-04-15 02:07:58 +0100

    [Combine] - Publish data once with Future and Just

--- 12-combine/learning-combine-with-swift /chapter-2/MyPlayground.playground/Contents.swift
+++ 12-combine/learning-combine-with-swift /chapter-2/MyPlayground.playground/Contents.swift
@@ -84,25 +84,75 @@ import UIKit
  */
 
 // (1) Declare an Int PassthroughSubject
+// let subject = PassthroughSubject<Int, Never>()
+//
+//// (2) Attach a subscriber to the subject
+// let subscription = subject
+//    .sink { print($0) }
+//
+//// (3) Publish the value '94', via the subject, directly
+// subject.send(94)
+//
+//// (4) Connect subject to a publisher, and publish the value '29'
+// Just(29)
+//    .subscribe(subject)
+//
+//// (5) Declare another subject, a CurrentValueSubject, with an initial 'I am a ...' value cached
+// let anotherSubject = CurrentValueSubject<String, Never>("I am a ...")
+//
+//// (6) Attach a subscriber to the subject
+// let anotherSubscription = anotherSubject
+//    .sink { print($0) }
+//
+//// (7) Publish the value 'Subject', via the subject, directly
+// anotherSubject.send("Subject")
+
+// MARK: - Just
+
+/*
+ * Omits a single result before termination or fail
+ * Wrap a primitive value into a publisher
+ */
+
+// MARK: - Future
+
+/*
+ * Result in single output or failure
+ * Wraps an async call
+ * Commonly used to make a single request/response
+ * Promise is a closure type that accepts a result consisting of a single value from a future, or failure
+ */
+
+let x = Just("A data stream")
+    .sink { value in
+        print("Value is: \(value)")
+    }
+
 let subject = PassthroughSubject<Int, Never>()
 
-// (2) Attach a subscriber to the subject
-let subscription = subject
-    .sink { print($0) }
-
-// (3) Publish the value '94', via the subject, directly
-subject.send(94)
-
-// (4) Connect subject to a publisher, and publish the value '29'
 Just(29)
     .subscribe(subject)
 
-// (5) Declare another subject, a CurrentValueSubject, with an initial 'I am a ...' value cached
-let anotherSubject = CurrentValueSubject<String, Never>("I am a ...")
+enum FutureError: Error {
+    case notMultiple
+}
 
-// (6) Attach a subscriber to the subject
-let anotherSubscription = anotherSubject
-    .sink { print($0) }
+let future = Future<String, FutureError> { promise in
+    let calendar = Calendar.current
+    let second = calendar.component(.second, from: Date())
+    print("second is \(second)")
+    if second.isMultiple(of: 3) {
+        promise(.success("We are successfull: \(second)"))
 
-// (7) Publish the value 'Subject', via the subject, directly
-anotherSubject.send("Subject")
+    } else {
+        promise(.failure(.notMultiple))
+    }
+}
+.catch { error in
+    Just("Caught an error: \(error)")
+}
+.delay(for: .init(1), scheduler: RunLoop.main)
+.eraseToAnyPublisher()
+
+future.sink(receiveCompletion: { print($0) },
+            receiveValue: { print($0) })
