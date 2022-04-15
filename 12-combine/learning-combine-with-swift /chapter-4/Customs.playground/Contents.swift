28cd894e6b51d7ec6b3c15cd1cbabc8428ee5efe
Mehmet Tarhan <hi@memtarhan.com>
2022-04-15 17:13:12 +0100

    [Combine] - Work with custom publishers and subscribers

--- 12-combine/learning-combine-with-swift /chapter-4/Customs.playground/Contents.swift
+++ 12-combine/learning-combine-with-swift /chapter-4/Customs.playground/Contents.swift
@@ -1,25 +1,28 @@
 import Combine
 import Foundation
 
-func isPrime<T: BinaryInteger>(_ n: T) -> T? {
-    guard n != 2 else { return n }
-    guard n % 2 != 0 && n > 1 else { return nil }
-
-    var i = 3
-    while i * i <= n {
-        if (Int(n) % i) == 0 {
-            return nil
-        }
-
-        i += 2
+extension Publisher {
+    func isPrimeInteger<T: BinaryInteger>() -> Publishers.CompactMap<Self, T> where Output == T {
+        compactMap { self.isPrime($0) }
     }
 
-    return n
+    func isPrime<T: BinaryInteger>(_ n: T) -> T? {
+        guard n != 2 else { return n }
+        guard n % 2 != 0 && n > 1 else { return nil }
+
+        var i = 3
+        while i * i <= n {
+            if (Int(n) % i) == 0 {
+                return nil
+            }
+
+            i += 2
+        }
+
+        return n
+    }
 }
 
 let numbers: [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-numbers.forEach { number in
-    if let primeValue = isPrime(number) {
-        print(primeValue)
-    }
-}
+numbers.publisher.isPrimeInteger()
+    .sink { print($0) }
