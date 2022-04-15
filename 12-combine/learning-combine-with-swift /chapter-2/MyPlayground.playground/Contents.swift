ec890075e6ff23f15a33d4a1783f386927ae8d70
Mehmet Tarhan <hi@memtarhan.com>
2022-04-15 01:32:07 +0100

    [Combine] - Consume data with subscribers

--- 12-combine/learning-combine-with-swift /chapter-2/MyPlayground.playground/Contents.swift
+++ 12-combine/learning-combine-with-swift /chapter-2/MyPlayground.playground/Contents.swift
@@ -1,5 +1,6 @@
 import Combine
 import UIKit
+import SwiftUI
 
 // let just = Just("Hello World!")
 //    .sink { value in
@@ -24,25 +25,44 @@ import UIKit
  */
 
 /// .map
+// [1, 5, 9]
+//    .publisher
+//    .map { $0 * $0 }
+//    .sink { print($0) }
+//
+// let url = URL(string: "https://jsonplaceholder.typicode.com/posts")!
+// struct Task: Decodable {
+//    let id: Int
+//    let title: String
+//    let userId: Int
+//    let body: String
+// }
+//
+// let dataPublisher = URLSession.shared.dataTaskPublisher(for: url)
+//    .map { $0.data }
+//    .decode(type: [Task].self, decoder: JSONDecoder())
+// let cancellableSink = dataPublisher
+//    .sink(receiveCompletion: { completion in
+//        print(completion)
+//    }) { tasks in
+//        print("Result \(String(describing: tasks.first?.title))")
+//    }
+
+// MARK: - Subscribers
+
+/*
+ * Contract to receive sequence of values
+ * Has two possible outcomes, input and failure
+ * Continues to receive as publisher transmits (demands)
+ * sink and .assing for receiving values
+ * Conform to AnyCancellable to cancel
+ */
+
 [1, 5, 9]
     .publisher
-    .map { $0 * $0 }
     .sink { print($0) }
 
-let url = URL(string: "https://jsonplaceholder.typicode.com/posts")!
-struct Task: Decodable {
-    let id: Int
-    let title: String
-    let userId: Int
-    let body: String
-}
-
-let dataPublisher = URLSession.shared.dataTaskPublisher(for: url)
-    .map { $0.data }
-    .decode(type: [Task].self, decoder: JSONDecoder())
-let cancellableSink = dataPublisher
-    .sink(receiveCompletion: { completion in
-        print(completion)
-    }) { tasks in
-        print("Result \(String(describing: tasks.first?.title))")
-    }
+let label = UILabel()
+Just("John")
+    .map { "My name is \($0)" }
+    .assign(to: \.text, on: label)
