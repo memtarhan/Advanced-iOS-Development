08ea3bb12baa3acf0f796c597cb3d1fb6293d912
Mehmet Tarhan <hi@memtarhan.com>
2022-04-15 01:44:28 +0100

    [Combine] - Publish and subscribe to data with subjects

--- 12-combine/learning-combine-with-swift /chapter-2/MyPlayground.playground/Contents.swift
+++ 12-combine/learning-combine-with-swift /chapter-2/MyPlayground.playground/Contents.swift
@@ -1,6 +1,7 @@
 import Combine
-import UIKit
+import PlaygroundSupport
 import SwiftUI
+import UIKit
 
 // let just = Just("Hello World!")
 //    .sink { value in
@@ -58,11 +59,50 @@ import SwiftUI
  * Conform to AnyCancellable to cancel
  */
 
-[1, 5, 9]
-    .publisher
+// [1, 5, 9]
+//    .publisher
+//    .sink { print($0) }
+//
+// let label = UILabel()
+// Just("John")
+//    .map { "My name is \($0)" }
+//    .assign(to: \.text, on: label)
+//
+// PlaygroundPage.current.liveView = label
+
+// MARK: - Subjects
+
+/*
+ * A publisher that exposes a method for outside callers to publish elements
+ * Commong for briding code from the old imperative world into modern, new Combine world
+ * .send() method used to emit select values to one or more subscribers
+ * As an aggregator for multiple subscribers, via subscriber demands signaling unlimited demand from connected publishers
+
+ * currentValueSibject persist initial state values for subscribers, unlike passthroughSubject
+ * Create publishers from objects which conform to ObservableObject
+
+ */
+
+// (1) Declare an Int PassthroughSubject
+let subject = PassthroughSubject<Int, Never>()
+
+// (2) Attach a subscriber to the subject
+let subscription = subject
     .sink { print($0) }
 
-let label = UILabel()
-Just("John")
-    .map { "My name is \($0)" }
-    .assign(to: \.text, on: label)
+// (3) Publish the value '94', via the subject, directly
+subject.send(94)
+
+// (4) Connect subject to a publisher, and publish the value '29'
+Just(29)
+    .subscribe(subject)
+
+// (5) Declare another subject, a CurrentValueSubject, with an initial 'I am a ...' value cached
+let anotherSubject = CurrentValueSubject<String, Never>("I am a ...")
+
+// (6) Attach a subscriber to the subject
+let anotherSubscription = anotherSubject
+    .sink { print($0) }
+
+// (7) Publish the value 'Subject', via the subject, directly
+anotherSubject.send("Subject")
