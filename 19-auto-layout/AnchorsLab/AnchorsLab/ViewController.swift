ab2db9b281ad40489a21c301b9db2bb484e3da4d
Mehmet Tarhan <hi@memtarhan.com>
2022-12-07 20:10:53 +0000

    [Auto Layout] - Adding a label to upper left

--- 19-auto-layout/AnchorsLab/AnchorsLab/ViewController.swift
+++ 19-auto-layout/AnchorsLab/AnchorsLab/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setupViews()
     }
 
+    private func setupViews() {
+        let upperLeftLabel = createLabel(withText: "upperLeft")
+        view.addSubview(upperLeftLabel)
 
+        upperLeftLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        upperLeftLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
+    }
+
+    private func createLabel(withText text: String) -> UILabel {
+        let label = UILabel()
+
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.backgroundColor = .systemMint
+
+        return label
+    }
 }
-
