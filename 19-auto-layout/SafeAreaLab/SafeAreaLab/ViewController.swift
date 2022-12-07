fd9fe1151336909fffd6ddf62ad18b388b166ffe
Mehmet Tarhan <hi@memtarhan.com>
2022-12-07 23:08:45 +0000

    [Auto Layout] - Adding a labels to bottom and top (safeAreaLayoutGuide)

--- 19-auto-layout/SafeAreaLab/SafeAreaLab/ViewController.swift
+++ 19-auto-layout/SafeAreaLab/SafeAreaLab/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setupViews()
     }
 
+    private func setupViews() {
+        let topLabel = createLabel(withText: "top")
+        view.addSubview(topLabel)
 
+        topLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        topLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+        
+        let bottomLabel = createLabel(withText: "bottom")
+        view.addSubview(bottomLabel)
+        
+        bottomLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        bottomLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+    }
+
+    private func createLabel(withText text: String) -> UILabel {
+        let label = UILabel()
+
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.font = .boldSystemFont(ofSize: 32)
+        label.backgroundColor = .systemMint
+
+        return label
+    }
 }
-
