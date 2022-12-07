2cee8c93560ee8307f6b754fa28011e0a33543f1
Mehmet Tarhan <hi@memtarhan.com>
2022-12-07 20:15:36 +0000

    [Auto Layout] - Adding a label to upper right

--- 19-auto-layout/AnchorsLab/AnchorsLab/ViewController.swift
+++ 19-auto-layout/AnchorsLab/AnchorsLab/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var safeAreaLayoutGuide: UILayoutGuide { view.safeAreaLayoutGuide }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,8 +20,14 @@ class ViewController: UIViewController {
         let upperLeftLabel = createLabel(withText: "upperLeft")
         view.addSubview(upperLeftLabel)
 
-        upperLeftLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
-        upperLeftLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
+        upperLeftLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        upperLeftLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 8).isActive = true
+
+        let upperRightLabel = createLabel(withText: "upperRight")
+        view.addSubview(upperRightLabel)
+
+        upperRightLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        upperRightLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -8).isActive = true
     }
 
     private func createLabel(withText text: String) -> UILabel {
