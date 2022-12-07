0ed5c56af6d03b8298bcbb382d80436fbabbdb16
Mehmet Tarhan <hi@memtarhan.com>
2022-12-07 20:10:41 +0000

    [Auto Layout] - Created AnchorsLab project

---
+++ 19-auto-layout/AnchorsLab/AnchorsLab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnchorsLab
+//
+//  Created by Mehmet Tarhan on 07/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
